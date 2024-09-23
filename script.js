e1178f4745d6a281d5573bac0f7bad0dde8070e6
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2024-09-23 22:00:32 +0200

    test

---
+++ script.js
@@ -0,0 +1,27 @@
+// const person = {
+//     name: 'Nika',
+//     age: 28
+// }
+
+// const copyPerson = person
+// copyPerson.love = 'programming'
+
+// console.table(person)
+// console.dir(person)
+// console.log(person.age)
+
+
+
+// let a = 5
+// for (; a < 7; a++) {
+//    console.log(a)
+// }
+
+const myDream = {
+    work: 'developer',
+    salary: 5000
+}
+
+myDream.happy = true
+
+console.log(myDream) 
