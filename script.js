bcc570f8304841e49e282b81d7658425e6810f73
Nika Donchenko <donchenko.veronika96@gmail.com>
2024-09-25 23:44:11 +0200

    func is object

--- script.js
+++ script.js
@@ -10,18 +10,25 @@
 // console.dir(person)
 // console.log(person.age)
 
-
-
 // let a = 5
 // for (; a < 7; a++) {
 //    console.log(a)
 // }
 
 const myDream = {
-    work: 'developer',
-    salary: 5000
+  work: 'developer',
+  salary: 5000,
+};
+
+myDream.happy = true;
+
+console.log(myDream);
+
+// fucn|object
+function myFn(a, b) {
+  let c;
+  a = a + 1;
+  c = a + b;
+  return c;
 }
-
-myDream.happy = true
-
-console.log(myDream) 
+console.dir(myFn);
